21ceb589ad93c31f64ff686115141389c5bfb7f3
esbatusadadda <juliakomorowicz16@gmail.com>
2024-08-28 11:25:12 +0700

    day one

---
+++ PHP-DASAR/tipe-data-number.php
@@ -0,0 +1,12 @@
+<?php
+echo "Decimal : ";
+var_dump(1234);
+echo "Octal : ";
+var_dump(0123);
+echo "Hexadecimal : ",
+var_dump(0x1A);
+echo "Binary : ";
+var_dump(0b111111111);
+echo "Underscore di number : ";
+var_dump(1_234_567)
+?>
